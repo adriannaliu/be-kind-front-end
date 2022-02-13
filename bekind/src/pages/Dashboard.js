4da005ae811694043918be8c79f6065d86798246
adriannaliu <72051204+adriannaliu@users.noreply.github.com>
2022-02-12 23:51:44 -0500

    added text box for add friend

--- bekind/src/pages/Dashboard.js
+++ bekind/src/pages/Dashboard.js
@@ -2,42 +2,49 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import DashboardTable from '../components/DashboardTable.js';
+import TextField from '@mui/material/TextField';
 
 const Dashboard = () => {
-  return (
-    <Box
-        component="form"
-        sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-    >
-        <h1 style={{color: '#E2725B'}}>
-            Hi, Emily!  
-        </h1>
-        <p>
-        “If you have a choice between being right and being kind..."
-        </p>
-        <h1>
-            BeKind.
-        </h1>
-        <h2 style={{color: '#E2725B'}}>
-            BeBuds
-        </h2>
-        <div>
-            <Button variant="contained" style={{backgroundColor: '#E2725B'}}>
-                Add Friend
-            </Button>
-        </div>
-        <br></br>
-        <DashboardTable />
-        <br></br>
-        <Button variant="contained" style={{backgroundColor: '#E2725B'}}>
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <h1 style={{ color: '#E2725B' }}>
+                Hi, Emily!
+            </h1>
+            <p>
+                “If you have a choice between being right and being kind..."
+            </p>
+            <h1>
+                BeKind.
+            </h1>
+            <div>
+                <h2 style={{ color: '#E2725B' }}>
+                    BeBuds
+                    <TextField 
+                        id="outlined-basic"
+                        size="small"
+                        variant="outlined"
+                        label="Username"
+                    />
+                    <Button variant="contained" style={{ backgroundColor: '#E2725B' }}>
+                        Add Friend
+                    </Button>
+                </h2>
+            </div>
+            <br></br>
+            <DashboardTable />
+            <br></br>
+            <Button variant="contained" style={{ backgroundColor: '#E2725B' }}>
                 Log Out
-        </Button>
-    </Box>
-  );
+            </Button>
+        </Box>
+    );
 }
 
 export default Dashboard;
